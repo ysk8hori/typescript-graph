c562b633fb206e00566b3597069992bd94f4e44d
ysk8hori <ysk8.hori@gmail.com>
2023-02-04 15:55:30 +0900

    refactor: add extract unique nodes function

--- src/createGraph.ts
+++ src/createGraph.ts
@@ -131,10 +131,20 @@ export function filter(
     );
   }
 
-  /** nodes もフィルタリングした影響で「relations にあるが nodes にない」が発生しているためマージする */
+  return {
+    nodes: extractUniqueNodes({ nodes: tmpNodes, relations: tmpRelations }),
+    relations: tmpRelations,
+  };
+}
+
+/**
+ * nodes と relations をマージしたユニークな node のリストを作り直す。
+ * フィルタリング処理などで「relations にあるが nodes にない」が発生したりするので必要。
+ */
+function extractUniqueNodes({ nodes, relations }: Graph): Node[] {
   const allNodes = [
-    ...tmpNodes,
-    ...tmpRelations.map(({ from, to }) => [from, to]).flat(),
+    ...nodes,
+    ...relations.map(({ from, to }) => [from, to]).flat(),
   ].reduce((pre, current) => {
     // 重複除去
     if (pre.some(node => isSameNode(node, current))) return pre;
@@ -142,5 +152,5 @@ export function filter(
     return pre;
   }, new Array<Node>());
 
-  return { nodes: allNodes, relations: tmpRelations };
+  return allNodes;
 }
