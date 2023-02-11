5f0097650317c8b9db3fe642c8419e5199cbb812
ysk8hori <ysk8.hori@gmail.com>
2023-02-11 20:06:54 +0900

    refactor: graph ディレクトリを作成

--- src/graph/createGraph.abstractionPath.spec.ts
+++ src/graph/createGraph.abstractionPath.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest';
 import { abstractionPath } from './createGraph';
-import { Node } from './models';
+import { Node } from '../models';
 
 test('/src/components/atoms/Button.tsx atoms = /src/components/atoms', () => {
   expect(
