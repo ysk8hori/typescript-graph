b61d024bcaed29806315afcf4fc4778fd8762306
ysk8hori <ysk8.hori@gmail.com>
2023-03-16 00:14:23 +0900

    feat: highlight 関数の実装

---
+++ src/graph/highlight.ts
@@ -0,0 +1,19 @@
+import { Graph, Node } from '../models';
+
+export function highlight(
+  highlightArray: string[] | undefined,
+  graph: Graph,
+): Graph {
+  if (!highlightArray || highlightArray.length === 0) return graph;
+  const nodes = graph.nodes.map<Node>(node => {
+    if (
+      highlightArray.some(word =>
+        node.path.toLowerCase().includes(word.toLowerCase()),
+      )
+    ) {
+      return { ...node, highlight: true };
+    }
+    return node;
+  });
+  return { ...graph, nodes };
+}
