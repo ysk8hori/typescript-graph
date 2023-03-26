7a860df0da0ba0fd78563607d3e70aff7ef93c9c
ysk8hori <ysk8.hori@gmail.com>
2023-03-27 00:50:06 +0900

    chore: replaceAll 使用のため lib を更新

--- dummy_project/src/includeFiles/a.ts
+++ dummy_project/src/includeFiles/a.ts
@@ -2,7 +2,7 @@ import childA from './children/childA';
 import a2 from './excludeFiles/g';
 import b2 from './excludeFiles/h';
 import c2 from './excludeFiles/i';
-import { style } from './excludeFiles/styles/style';
+import { style } from './excludeFiles/style/style';
 
 export default function a() {
   childA();
