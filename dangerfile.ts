3b9ab3e44d4e9237f35bd6849a962c49f1e4457f
ysk8hori <ysk8.hori@gmail.com>
2023-03-27 22:53:42 +0900

    chore: danger に tsg ファイルを添付させる

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,12 @@
-import { danger, warn } from 'danger';
+import { danger, markdown, warn } from 'danger';
+import { $ } from 'zx';
 
 //  なぜ変更したのかの説明を含めると、どんなPRも小さくはありません
 if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
+
+const fileName = './typescript-graph.md';
+await $`tsg --md "${fileName}"`;
+
+markdown('hoge', fileName);
