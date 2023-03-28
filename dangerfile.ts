78bd42e38bf85a25dbc3e28d283fb522f2a83b73
ysk8hori <ysk8.hori@gmail.com>
2023-03-28 22:07:58 +0900

    feat: 変更箇所を include とする

--- dangerfile.ts
+++ dangerfile.ts
@@ -7,7 +7,15 @@ if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
 
+const modified = danger.git.modified_files;
+const created = danger.git.created_files;
+const deleted = danger.git.deleted_files;
+
 const fileName = './typescript-graph.md';
-execSync(`npx tsg --md "${fileName}" --LR`);
+execSync(
+  `npx tsg --md "${fileName}" --LR --include ${modified.join(
+    ' ',
+  )} ${created.join(' ')} ${deleted.join(' ')}} --exclude node_modules`,
+);
 const graphString = readFileSync(fileName, 'utf8');
 message(graphString);
