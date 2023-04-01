af0bd2d43bbfdc78b10afb153089bf54d2f97ea7
ysk8hori <ysk8.hori@gmail.com>
2023-04-01 11:53:44 +0900

    feat: Relation の比較で kind を考慮する

--- src/models.ts
+++ src/models.ts
@@ -37,7 +37,9 @@ export function isSameNode(a: Node, b: Node): boolean {
   return a.path === b.path;
 }
 export function isSameRelation(a: Relation, b: Relation): boolean {
-  return isSameNode(a.from, b.from) && isSameNode(a.to, b.to);
+  return (
+    a.kind === b.kind && isSameNode(a.from, b.from) && isSameNode(a.to, b.to)
+  );
 }
 /** 受け取った relation の重複をなくす */
 export function getUniqueRelations(relations: Relation[]): Relation[] {
