eff8449ee4f06413c1be2bb29687d60fea28cacd
ysk8hori <ysk8.hori@gmail.com>
2023-04-25 22:36:03 +0900

    fix(abstraction): 抽象化できないケースがあったので修正

---
+++ src/graph/abstraction.spec.ts
@@ -0,0 +1,49 @@
+import { it, expect } from 'vitest';
+import { abstraction } from './abstraction';
+
+it('指定したディレクトリを抽象化できる', () => {
+  expect(
+    abstraction(['src/a/b', 'src/a/b/c'], {
+      nodes: [
+        {
+          path: 'src/a/a.ts',
+          name: 'a.ts',
+          changeStatus: 'not_modified',
+        },
+        {
+          path: 'src/a/b/b.ts',
+          name: 'b.ts',
+          changeStatus: 'not_modified',
+        },
+        {
+          path: 'src/a/b/c/c.ts',
+          name: 'c.ts',
+          changeStatus: 'not_modified',
+        },
+        {
+          path: 'src/a/b/c/d/d.ts',
+          name: 'd.ts',
+          changeStatus: 'not_modified',
+        },
+      ],
+      relations: [],
+    }),
+  ).toMatchInlineSnapshot(`
+    {
+      "nodes": [
+        {
+          "changeStatus": "not_modified",
+          "name": "a.ts",
+          "path": "src/a/a.ts",
+        },
+        {
+          "changeStatus": "not_modified",
+          "isDirectory": true,
+          "name": "/b",
+          "path": "src/a/b",
+        },
+      ],
+      "relations": [],
+    }
+  `);
+});
