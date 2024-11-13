b51e0f3fa460b7e1629f8bc12e5c09bfbab34f14
ysk8hori <ysk8.hori@gmail.com>
2024-11-13 21:00:51 +0900

    fix(abstraction): abstraction でファイルを指定した際の出力でファイルがdirの見た目になるバグを修正

--- src/graph/abstraction.spec.ts
+++ src/graph/abstraction.spec.ts
@@ -47,3 +47,50 @@ it('指定したディレクトリを抽象化できる', () => {
     }
   `);
 });
+
+it('ファイル名を指定した場合、その対象はディレクトリ扱いにならない', () => {
+  expect(
+    abstraction(['src/a/b', 'src/a/b/c', 'src/a/a.ts'], {
+      nodes: [
+        {
+          path: 'src/a/a.ts',
+          name: 'a.ts',
+          changeStatus: 'not_modified',
+        },
+        {
+          path: 'src/a/b/b.ts',
+          name: 'b.ts',
+          changeStatus: 'not_modified',
+        },
+        {
+          path: 'src/a/b/c/c.ts',
+          name: 'c.ts',
+          changeStatus: 'not_modified',
+        },
+        {
+          path: 'src/a/b/c/d/d.ts',
+          name: 'd.ts',
+          changeStatus: 'not_modified',
+        },
+      ],
+      relations: [],
+    }),
+  ).toMatchInlineSnapshot(`
+    {
+      "nodes": [
+        {
+          "changeStatus": "not_modified",
+          "name": "a.ts",
+          "path": "src/a/a.ts",
+        },
+        {
+          "changeStatus": "not_modified",
+          "isDirectory": true,
+          "name": "/b",
+          "path": "src/a/b",
+        },
+      ],
+      "relations": [],
+    }
+  `);
+});
