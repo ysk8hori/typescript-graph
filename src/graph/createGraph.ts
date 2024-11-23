800277d1c688a9ea0b76fd8599d409d2a30719ad
ysk8hori <ysk8.hori@gmail.com>
2024-11-24 00:39:08 +0900

    refactor(creategraph): ソースファイルをグラフにする部分を関数化

--- src/graph/createGraph.ts
+++ src/graph/createGraph.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import * as ts from 'typescript';
 import { Graph, Meta, Node, OptionValues, Relation } from '../models';
 import { pipe, piped } from 'remeda';
+import { mergeGraph } from './utils';
 
 export function createGraph(
   /**
@@ -30,90 +31,23 @@ export function createGraph(
   );
   options.rootDir = rootDir;
   const program = ts.createProgram(fileNames, options);
-  const nodes: Node[] = [];
-  const relations: Relation[] = [];
+
   const bindWords_isFileNameMatchSomeWords =
     (array: string[]) => (filename: string) =>
       array.some(word => filename.includes(word));
-
   const isMatchSomeExclude = opt.exclude
     ? bindWords_isFileNameMatchSomeWords(opt.exclude)
     : () => false;
   const isNotMatchSomeExclude = (filename: string) =>
     !isMatchSomeExclude(filename);
-  function getFilePath(sourceFile: ts.SourceFile) {
-    return options.rootDir
-      ? sourceFile.fileName.replace(options.rootDir + '/', '')
-      : sourceFile.fileName;
-  }
 
-  program
+  const graphs = program
     .getSourceFiles()
-    .filter(sourceFile => !sourceFile.fileName.includes('node_modules'))
-    .filter(piped(getFilePath, removeSlash, isNotMatchSomeExclude))
-    .forEach(sourceFile => {
-      const filePath = pipe(sourceFile, getFilePath, removeSlash);
-      const fileName = getName(filePath);
-      const fromNode: Node = {
-        path: filePath,
-        name: fileName,
-        changeStatus: 'not_modified',
-      };
-      nodes.push(fromNode);
+    .filter(sourceFile => !sourceFile.fileName.includes('node_modules')) // node_modules 配下のファイルは除外
+    .filter(piped(getFilePath(options), removeSlash, isNotMatchSomeExclude))
+    .map(analyzeSoucreFile(options));
 
-      ts.forEachChild(sourceFile, node => {
-        const importPaths: (string | undefined)[] = [];
-        function getModuleNameText(node: ts.Node) {
-          if (ts.isImportDeclaration(node)) {
-            importPaths.push(node.moduleSpecifier?.getText(sourceFile));
-          } else if (ts.isCallExpression(node)) {
-            const text = node.getText(sourceFile);
-            if (text.includes('require') || text.includes('import')) {
-              importPaths.push(node.arguments[0]?.getText(sourceFile));
-            }
-          } else if (ts.isExportDeclaration(node)) {
-            importPaths.push(node.moduleSpecifier?.getText(sourceFile));
-          }
-          ts.forEachChild(node, getModuleNameText);
-        }
-        getModuleNameText(node);
-
-        importPaths.forEach(moduleNameText => {
-          if (!moduleNameText) return;
-          const moduleName = moduleNameText.slice(1, moduleNameText.length - 1); // import 文のクォート及びダブルクォートを除去
-          const moduleFileFullName =
-            ts.resolveModuleName(
-              moduleName,
-              sourceFile.fileName,
-              options,
-              ts.sys,
-            ).resolvedModule?.resolvedFileName ?? '';
-          const moduleFilePath = removeSlash(
-            options.rootDir
-              ? moduleFileFullName.replace(options.rootDir, '')
-              : moduleFileFullName,
-          );
-          if (!moduleFilePath) return;
-          const toNode: Node = {
-            path: moduleFilePath,
-            name: getName(moduleFilePath),
-            changeStatus: 'not_modified',
-          };
-          if (!findNode(nodes, moduleFilePath)) {
-            nodes.push(toNode);
-          }
-          relations.push({
-            kind: 'depends_on',
-            from: fromNode,
-            to: toNode,
-            fullText: node.getChildAt(1, sourceFile)?.getText(sourceFile) ?? '',
-            changeStatus: 'not_modified',
-          });
-        });
-      });
-    });
-
-  return { graph: { nodes, relations }, meta: { rootDir } };
+  return { graph: mergeGraph(...graphs), meta: { rootDir } };
 }
 
 function getName(filePath: string) {
@@ -140,4 +74,94 @@ function findNode(nodes: Node[], filePath: string): Node | undefined {
 
 function removeSlash(pathName: string): string {
   return pathName.startsWith('/') ? pathName.replace('/', '') : pathName;
+}
+
+function getFilePath(
+  options: ts.CompilerOptions,
+): (sourceFile: ts.SourceFile) => string {
+  return (sourceFile: ts.SourceFile) =>
+    options.rootDir
+      ? sourceFile.fileName.replace(options.rootDir + '/', '')
+      : sourceFile.fileName;
+}
+
+function analyzeSoucreFile(
+  options: ts.CompilerOptions,
+): (sourceFile: ts.SourceFile) => Graph {
+  return (sourceFile: ts.SourceFile) => {
+    const nodes: Node[] = [];
+    const relations: Relation[] = [];
+    const filePath = pipe(sourceFile, getFilePath(options), removeSlash);
+    const fileName = getName(filePath);
+    const fromNode: Node = {
+      path: filePath,
+      name: fileName,
+      changeStatus: 'not_modified',
+    };
+    nodes.push(fromNode);
+
+    ts.forEachChild(sourceFile, node => {
+      const importPaths: (string | undefined)[] = [];
+      function getModuleNameText(node: ts.Node) {
+        if (ts.isImportDeclaration(node)) {
+          console.log(
+            'isImportDeclaration',
+            node.moduleSpecifier?.getText(sourceFile),
+          );
+          importPaths.push(node.moduleSpecifier?.getText(sourceFile));
+        } else if (ts.isCallExpression(node)) {
+          const text = node.getText(sourceFile);
+          if (text.includes('require') || text.includes('import')) {
+            importPaths.push(node.arguments[0]?.getText(sourceFile));
+          }
+        } else if (ts.isExportDeclaration(node)) {
+          importPaths.push(node.moduleSpecifier?.getText(sourceFile));
+        }
+        ts.forEachChild(node, getModuleNameText);
+      }
+      getModuleNameText(node);
+      console.log(importPaths);
+
+      importPaths.forEach(moduleNameText => {
+        if (!moduleNameText) {
+          console.log('moduleNameText is empty');
+          return;
+        }
+        const moduleName = moduleNameText.slice(1, moduleNameText.length - 1); // import 文のクォート及びダブルクォートを除去
+        console.log(
+          'ts.resolveModuleName(moduleName, sourceFile.fileName, options, ts.sys).resolvedModule?.resolvedFileName = ',
+          ts.resolveModuleName(moduleName, sourceFile.fileName, options, ts.sys)
+            .resolvedModule?.resolvedFileName,
+        );
+        const moduleFileFullName =
+          ts.resolveModuleName(moduleName, sourceFile.fileName, options, ts.sys)
+            .resolvedModule?.resolvedFileName ?? '';
+        const moduleFilePath = removeSlash(
+          options.rootDir
+            ? moduleFileFullName.replace(options.rootDir, '')
+            : moduleFileFullName,
+        );
+        if (!moduleFilePath) {
+          console.log('moduleFilePath is empty');
+          return;
+        }
+        const toNode: Node = {
+          path: moduleFilePath,
+          name: getName(moduleFilePath),
+          changeStatus: 'not_modified',
+        };
+        if (!findNode(nodes, moduleFilePath)) {
+          nodes.push(toNode);
+        }
+        relations.push({
+          kind: 'depends_on',
+          from: fromNode,
+          to: toNode,
+          fullText: node.getChildAt(1, sourceFile)?.getText(sourceFile) ?? '',
+          changeStatus: 'not_modified',
+        });
+      });
+    });
+    return { nodes, relations };
+  };
 }
