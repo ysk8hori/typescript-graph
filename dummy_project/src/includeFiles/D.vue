efb072c81d965d3fd5152f872b4fa9cf6d1b7d84
ysk8hori <ysk8.hori@gmail.com>
2024-11-24 18:38:07 +0900

    test(integration): dummy_project に vue ファイルを追加

---
+++ dummy_project/src/includeFiles/D.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
