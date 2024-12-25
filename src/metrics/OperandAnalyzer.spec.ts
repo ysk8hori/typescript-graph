0d16f8e339e35d3c5e0de63b4e0c838564104f7e
ysk8hori <ysk8.hori@gmail.com>
2024-12-25 10:40:38 +0900

    test(operandanalyzer): テスト追加

--- src/metrics/OperandAnalyzer.spec.ts
+++ src/metrics/OperandAnalyzer.spec.ts
@@ -9,11 +9,13 @@ function createSourceFile(sourceCode: string) {
     sourceCode,
     ts.ScriptTarget.Latest,
     true,
+    ts.ScriptKind.TSX,
   );
 }
 
 test.each([
   [
+    '代入と連鎖代入',
     'let x = 10;let y = x = 20;',
     {
       operandsTotal: 5,
@@ -21,7 +23,7 @@ test.each([
     },
   ],
   [
-    // 関数名も operand に含まれる
+    '関数名も operand に含む',
     'function add(x: number, y = 1) {return x + y;}',
     {
       operandsTotal: 6,
@@ -29,19 +31,38 @@ test.each([
     },
   ],
   [
-    // else をカウントできる
-    'let x = 10, y=1;if (x > 5) {x += 1;} else {x = 0;}',
+    '変数宣言リスト',
+    'let x=10,y=1;if(x>5){x+=1;}else{x=0;}',
     {
       operandsTotal: 10,
       operandsUnique: 6,
     },
   ],
-  // ["import { test, expect, describe } from 'vitest';test();", 4],
-
-  // [
-  //   6,
-  // ],
-])('`%s`', (sourceCode, expected) => {
+  [
+    'import 対象のモジュールとインポート元のパスもカウントする',
+    "import { test, expect, describe } from 'vitest';test();",
+    {
+      operandsTotal: 5,
+      operandsUnique: 4,
+    },
+  ],
+  [
+    '型名と型定義におけるプロパティ名もカウントする',
+    `\
+interface Swordsman {
+  sword: string;
+}
+interface Wizard {
+  magic: string;
+}
+type MagicalSwordsman = Swordsman & Wizard;
+    `,
+    {
+      operandsTotal: 7,
+      operandsUnique: 5,
+    },
+  ],
+])('%s', (_, sourceCode, expected) => {
   logAstNodes(sourceCode);
   const analyzer = new OperandAnalyzer(createSourceFile(sourceCode));
   expect(analyzer.analyze()).toEqual(expected);
