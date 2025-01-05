8b7b9e4a9d317b7250d07b1c4ba62b5296e86e32
ysk8hori <ysk8.hori@gmail.com>
2025-01-05 23:02:24 +0900

    refactor: delta で使う関数をトップレベルから export する

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * from './feature/graph/models';
+export { createGraph } from './feature/graph/createGraph';
+export { abstraction } from './feature/graph/abstraction';
+export { filterGraph } from './feature/graph/filterGraph';
+export { mergeGraph } from './feature/graph/utils';
+export { mermaidify } from './feature/mermaid/mermaidify';
+
+// TODO: metric 関連
