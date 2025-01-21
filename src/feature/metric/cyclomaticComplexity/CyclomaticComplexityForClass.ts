cbd53c8cc9a45953eb887b39cab9d2d215400aa6
ysk8hori <ysk8.hori@gmail.com>
2025-01-21 22:51:27 +0900

    refactor(metric): HierarchicalMetrics なクラスの生成処理を関数化

--- src/feature/metric/cyclomaticComplexity/CyclomaticComplexityForClass.ts
+++ src/feature/metric/cyclomaticComplexity/CyclomaticComplexityForClass.ts
@@ -3,18 +3,17 @@ import { ClassVisitorFactory } from '../VisitorFactory';
 import CyclomaticComplexityAnalyzer from './CyclomaticComplexityAnalyzer';
 import CyclomaticComplexityForNormalNode from './CyclomaticComplexityForNormalNode';
 
+const create = (name: string, scope: MetricsScope) =>
+  new CyclomaticComplexityForNormalNode(name, scope);
+
 export default class CyclomaticComplexityForClass extends CyclomaticComplexityAnalyzer {
   constructor(name: string, scope: MetricsScope) {
     super(name, scope, {
       visitorFactory: new ClassVisitorFactory({
-        createGetAccessorVisitor: (name, scope) =>
-          new CyclomaticComplexityForNormalNode(name, scope),
-        createSetAccessorVisitor: (name, scope) =>
-          new CyclomaticComplexityForNormalNode(name, scope),
-        createMethodVisitor: (name, scope) =>
-          new CyclomaticComplexityForNormalNode(name, scope),
-        createConstructorVisitor: (name, scope) =>
-          new CyclomaticComplexityForNormalNode(name, scope),
+        createGetAccessorVisitor: create,
+        createSetAccessorVisitor: create,
+        createMethodVisitor: create,
+        createConstructorVisitor: create,
       }),
     });
   }
